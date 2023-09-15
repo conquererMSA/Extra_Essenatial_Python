e9bfb4d8c3a73c244f3f870161dd8e9b0e0da090
140373331+conquererMSA@users.noreply.github.com <140373331+conquererMSA@users.noreply.github.com>
2023-09-15 21:56:10 +0600

    Ternery Operator

---
+++ TernaryOperator.py
@@ -0,0 +1,14 @@
+'''
+Unary Operators works on 1 operands
+Binary Operators works on 2 operands
+Ternery Operators works on 3 operands
+Python e ternery operator er syntex ekdomi alada. Python e shorthand if -else
+Ternery operator hisabe use hoy
+structure: assignVariable= operand1 if condition/operand2 else operand 3
+'''
+age=20
+# message='You can vote' if(age>=18) else 'You cant vote'
+# print(message)
+have=True
+message='You have money' if(age>=21 and have==True) else 'You dont have money'
+print(message)
