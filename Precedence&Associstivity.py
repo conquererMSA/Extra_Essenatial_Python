b9d590d91d723caffc89e67ffab9d85f5d66075e
140373331+conquererMSA@users.noreply.github.com <140373331+conquererMSA@users.noreply.github.com>
2023-09-15 20:48:33 +0600

    6 Precedency and Associative operator explained

---
+++ Precedence&Associstivity.py
@@ -0,0 +1,32 @@
+'''
+Zokhon kuno python expression and statement e ekadik operators thake tokhon sothik
+result pete somossya hoy...
+Precedency roler sahazye amra python expression theke sothik result pete pari.
+(2*x-6*y) ekti python expression, priority ba precedency role diye er sothik result
+pawya zabe.
+***Precedence role nirdharon expression er kun operator protome exicute hobe.
+4+3*2, precedence role onuzayee ei expression e protome multiplication operator
+exicute hobe and tar result hobe 6 then 6 er sathe 4 addition hobe.
+
+#associativity: zokhon duti operator er precedency same hoy, tokhon associativity
+nirdharon kore kun operator protome exicute/evaluated hobe.
+2+6*4/5, ei expression e 6 er sathe 4 er multiply and 4 er sathe 5 er division er
+priority/precedency same.
+Dui doroner associativity pawya zay:
+1. Left to right associativity
+2. Right to left associativity(**, unary, assignment)
+
+2*3//5, ekhane duti opertoer er priority same, * and // operator associativity right
+to left. Orthat 2*3//5 => 6//5 =>1 hobe,
+--------------------------------------------------
+(2+1)*2**4//3+4 paranthesis
+3*2**4//3+4    exponentiation
+3*16//3+4 multiplication
+48//3+4  division
+16+4  addition
+20
+----------------------------
+2**3**4, ekhane dutui same operator, tader priprity o same. Kintu exponentiation,
+negation, and and assignmnet operator ** right to left associative order follow kore.
+ei exxpression e 3 er power hobe 4, er result hobe 2 er power....
+'''
